2b5e52fcc365c21de3617fcaf3f549cd8fdabc27
WiemKhlifi <w.khlifi@instadeep.com>
2023-12-05 14:00:32 +0000

    feat: add files for exp

--- mava/systems/ff_mappo_lbf.py
+++ mava/systems/ff_mappo_lbf.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import copy
 import time
 from typing import Any, Dict, Sequence, Tuple
@@ -45,6 +44,7 @@ from mava.types import (
     LearnerFn,
     LearnerState,
     Observation,
+    ObservationGlobalState,
     OptStates,
     Params,
     PPOTransition,
@@ -52,7 +52,7 @@ from mava.types import (
 from mava.utils.checkpointing import Checkpointer
 from mava.utils.jax import merge_leading_dims
 from mava.wrappers.jumanji import LbfWrapper
-from mava.wrappers.shared import AgentIDWrapper, LogWrapper
+from mava.wrappers.shared import AgentIDWrapper, GlobalStateWrapper, LogWrapper
 
 
 class Actor(nn.Module):
@@ -89,11 +89,11 @@ class Critic(nn.Module):
     """Critic Network."""
 
     @nn.compact
-    def __call__(self, observation: Observation) -> chex.Array:
+    def __call__(self, observation: ObservationGlobalState) -> chex.Array:
         """Forward pass."""
 
         critic_output = nn.Dense(128, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0))(
-            observation.agents_view
+            observation.global_state
         )
         critic_output = nn.relu(critic_output)
         critic_output = nn.Dense(128, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0))(
@@ -115,7 +115,7 @@ def get_learner_fn(
 ) -> LearnerFn[LearnerState]:
     """Get the learner function."""
 
-    # Get apply and update functions for actor and critic networks.
+    # Unpack apply and update functions.
     actor_apply_fn, critic_apply_fn = apply_fns
     actor_update_fn, critic_update_fn = update_fns
 
@@ -312,7 +312,6 @@ def get_learner_fn(
                 )
                 critic_new_params = optax.apply_updates(params.critic_params, critic_updates)
 
-                # PACK NEW PARAMS AND OPTIMISER STATE
                 new_params = Params(actor_new_params, critic_new_params)
                 new_opt_state = OptStates(actor_new_opt_state, critic_new_opt_state)
 
@@ -376,7 +375,7 @@ def get_learner_fn(
         Args:
             learner_state (NamedTuple):
                 - params (Params): The initial model parameters.
-                - opt_states (OptStates): The initial optimizer state.
+                - opt_states (OptStates): The initial optimizer states.
                 - rng (chex.PRNGKey): The random number generator state.
                 - env_state (LogEnvState): The environment state.
                 - timesteps (TimeStep): The initial timestep in the initial trajectory.
@@ -428,9 +427,15 @@ def learner_setup(
         optax.adam(config["system"]["critic_lr"], eps=1e-5),
     )
 
-    # Initialise observation: Select only obs for a single agent.
-    init_x = env.observation_spec().generate_value()
-    init_x = jax.tree_util.tree_map(lambda x: x[0], init_x)
+    # Initialise observation.
+    obs = env.observation_spec().generate_value()
+    # Select only obs for a single agent.
+    init_x = ObservationGlobalState(
+        agents_view=obs.agents_view[0],
+        action_mask=obs.action_mask[0],
+        global_state=obs.global_state[0],
+        step_count=obs.step_count[0],
+    )
     init_x = jax.tree_util.tree_map(lambda x: x[None, ...], init_x)
 
     # Initialise actor params and optimiser state.
@@ -508,16 +513,16 @@ def run_experiment(_config: Dict) -> None:
     # Create envs
     generator = RandomGenerator(**config["env"]["scenario"]["task_config"])
     env = jumanji.make(config["env"]["env_name"], generator=generator)
-    env = LbfWrapper(env=env)
+    env = GlobalStateWrapper(LbfWrapper(env))
     # Add agent id to observation.
     if config["system"]["add_agent_id"]:
-        env = AgentIDWrapper(env)
+        env = AgentIDWrapper(env=env, has_global_state=True)
     env = AutoResetWrapper(env)
     env = LogWrapper(env)
     eval_env = jumanji.make(config["env"]["env_name"], generator=generator)
-    eval_env = LbfWrapper(eval_env)
+    eval_env = GlobalStateWrapper(LbfWrapper(eval_env))
     if config["system"]["add_agent_id"]:
-        eval_env = AgentIDWrapper(eval_env)
+        eval_env = AgentIDWrapper(env=eval_env, has_global_state=True)
 
     # PRNG keys.
     rng, rng_e, rng_p = jax.random.split(jax.random.PRNGKey(config["system"]["seed"]), num=3)
@@ -537,7 +542,6 @@ def run_experiment(_config: Dict) -> None:
     # Calculate total timesteps.
     n_devices = len(jax.devices())
     config["arch"]["devices"] = jax.devices()
-
     config["system"]["num_updates_per_eval"] = (
         config["system"]["num_updates"] // config["arch"]["num_evaluation"]
     )
@@ -585,7 +589,6 @@ def run_experiment(_config: Dict) -> None:
     for i in range(config["arch"]["num_evaluation"]):
         # Train.
         start_time = time.time()
-
         learner_output = learn(learner_state)
         jax.block_until_ready(learner_output)
 
@@ -656,7 +659,7 @@ def run_experiment(_config: Dict) -> None:
         )
 
 
-@hydra.main(config_path="../configs", config_name="default_ff_ippo_lbf.yaml", version_base="1.2")
+@hydra.main(config_path="../configs", config_name="default_ff_mappo_lbf.yaml", version_base="1.2")
 def hydra_entry_point(cfg: DictConfig) -> None:
     """Experiment entry point."""
     # Convert config to python dict.
@@ -665,7 +668,7 @@ def hydra_entry_point(cfg: DictConfig) -> None:
     # Run experiment.
     run_experiment(cfg)
 
-    print(f"{Fore.CYAN}{Style.BRIGHT}IPPO experiment completed{Style.RESET_ALL}")
+    print(f"{Fore.CYAN}{Style.BRIGHT}MAPPO experiment completed{Style.RESET_ALL}")
 
 
 if __name__ == "__main__":
---
+++ run.sh
@@ -0,0 +1,1 @@
+python mava/systems/ff_ippo_lbf.py -m system.seed=0,1,2,3,4,5,6,7,8,9 env/scenario=2s-8x8-2p-2f-coop,8x8-2p-2f-coop,2s-10x10-3p-3f,10x10-3p-3f,15x15-3p-5f,15x15-4p-3f,15x15-4p-5f && python mava/systems/ff_mappo_lbf.py -m system.seed=0,1,2,3,4,5,6,7,8,9 env/scenario=2s-8x8-2p-2f-coop,8x8-2p-2f-coop,2s-10x10-3p-3f,10x10-3p-3f,15x15-3p-5f,15x15-4p-3f,15x15-4p-5f
