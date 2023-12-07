ba1df9190e38164c90d4fd303f415740af13b0d8
Ruan de Kock <ruanjdekock@gmail.com>
2023-12-07 15:24:46 +0000

    feat: experiment setup

---
+++ run_exp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python mava/systems/ff_ippo.py -m env=lbf env/scenario=2s-8x8-2p-2f-coop,8x8-2p-2f-coop,2s-10x10-3p-3f,10x10-3p-3f,15x15-4p-3f,15x15-4p-5f,15x15-3p-5f && python mava/systems/ff_mappo.py -m env=lbf env/scenario=2s-8x8-2p-2f-coop,8x8-2p-2f-coop,2s-10x10-3p-3f,10x10-3p-3f,15x15-4p-3f,15x15-4p-5f,15x15-3p-5f && python mava/systems/rec_ippo.py -m env=lbf env/scenario=2s-8x8-2p-2f-coop,8x8-2p-2f-coop,2s-10x10-3p-3f,10x10-3p-3f,15x15-4p-3f,15x15-4p-5f,15x15-3p-5f && python mava/systems/rec_mappo.py -m env=lbf env/scenario=2s-8x8-2p-2f-coop,8x8-2p-2f-coop,2s-10x10-3p-3f,10x10-3p-3f,15x15-4p-3f,15x15-4p-5f,15x15-3p-5f
