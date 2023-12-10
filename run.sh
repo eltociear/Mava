1a4ea43cd954dee9bc2878bce842a550b57699a7
WiemKhlifi <w.khlifi@instadeep.com>
2023-12-10 16:23:51 +0000

    small edits

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python mava/systems/ff_mappo.py -m system.seed=0,1 env/scenario=15x15-3p-5f,15x15-4p-3f,15x15-4p-5f
+python mava/systems/ff_mappo.py -m system.seed=0,1,2,3,4,5,6,7,8,9 env/scenario=15x15-3p-5f,15x15-4p-3f,15x15-4p-5f
