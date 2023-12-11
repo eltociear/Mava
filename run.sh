f6bc3fee061ecc15c9352d279e5c49cbc69bea78
Ruan de Kock <ruanjdekock@gmail.com>
2023-12-11 18:49:45 +0200

    chore: fix run script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 python mava/systems/ff_ippo.py -m system.seed=0,1,2,3,4,5,6,7,8,9 env=rware env/scenario=tiny-2ag,tiny-4ag,small-4ag && \
 python mava/systems/ff_mappo.py -m system.seed=0,1,2,3,4,5,6,7,8,9 env=rware env/scenario=tiny-2ag,tiny-4ag,small-4ag && \
 python mava/systems/rec_ippo.py -m system.seed=0,1,2,3,4,5,6,7,8,9 env=rware env/scenario=tiny-2ag,tiny-4ag,small-4ag && \
-python mava/systems/rec_mappo.py -m system.seed=0,1,2,3,4,5,6,7,8,9 env=rware env/scenario=tiny-2ag,tiny-4ag,small-4ag && 
+python mava/systems/rec_mappo.py -m system.seed=0,1,2,3,4,5,6,7,8,9 env=rware env/scenario=tiny-2ag,tiny-4ag,small-4ag
