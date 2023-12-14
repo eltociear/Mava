5af8710274bb83281b212d48a39f9d8a5d942f57
Ruan de Kock <ruanjdekock@gmail.com>
2023-12-14 12:40:36 +0200

    chore: fix typo in run script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python mava/systems/ff_ippo.py -m system.seed=0,1,2,3,4,5,6,7,8,9 mava_config/env/scenario=small-4ag,tiny-4ag,tiny-2ag
+python mava/systems/ff_ippo.py -m system.seed=0,1,2,3,4,5,6,7,8,9 env/scenario=small-4ag,tiny-4ag,tiny-2ag
