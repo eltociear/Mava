118824c10c4fc6371efe3bce44e0e1a5cd3d6b3a
WiemKhlifi <w.khlifi@instadeep.com>
2023-12-15 09:01:16 +0000

    config edits run 16 envs

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-python mava/systems/ff_mappo.py -m system.seed=0,1,2,3,4,5,6,7,8,9 env/scenario=15x15-3p-5f,15x15-4p-3f,15x15-4p-5f
+python mava/systems/ff_ippo.py -m system.seed=0,1,2,3 env/scenario=2s-8x8-2p-2f-coop,15x15-4p-3f &&
+python mava/systems/ff_mappo.py -m system.seed=0,1,2,3 env/scenario=2s-8x8-2p-2f-coop,15x15-4p-3f &&
+python mava/systems/rec_mappo.py -m system.seed=0,1,2,3 env/scenario=2s-8x8-2p-2f-coop,15x15-4p-3f &&
+python mava/systems/rec_ippo.py -m system.seed=0,1,2,3 env/scenario=2s-8x8-2p-2f-coop,15x15-4p-3f
