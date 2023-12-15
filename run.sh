44f3df936e7cbe342dedcbbbe5497a5bca98518b
OmaymaMahjoub <omaymamahjoub98@gmail.com>
2023-12-15 10:26:24 +0100

    feat: exp prep

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python mava/systems/ff_ippo.py -m system.seed=0,1,2,3 env/scenario=2s-8x8-2p-2f-coop,15x15-4p-3f &&
-python mava/systems/ff_mappo.py -m system.seed=0,1,2,3 env/scenario=2s-8x8-2p-2f-coop,15x15-4p-3f &&
+#python mava/systems/ff_ippo.py -m system.seed=0,1,2,3 env/scenario=2s-8x8-2p-2f-coop,15x15-4p-3f &&
+#python mava/systems/ff_mappo.py -m system.seed=0,1,2,3 env/scenario=2s-8x8-2p-2f-coop,15x15-4p-3f &&
 python mava/systems/rec_mappo.py -m system.seed=0,1,2,3 env/scenario=2s-8x8-2p-2f-coop,15x15-4p-3f &&
 python mava/systems/rec_ippo.py -m system.seed=0,1,2,3 env/scenario=2s-8x8-2p-2f-coop,15x15-4p-3f
